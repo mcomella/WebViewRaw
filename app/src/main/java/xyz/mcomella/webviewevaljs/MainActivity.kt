edc40102734abb2928dc1a2bb0c0032a05ee85c2
Chenxia Liu <liuche@mozilla.com>
2018-07-20 13:36:32 -0700

    Add fullscreen handling to chromeclient.

--- app/src/main/java/xyz/mcomella/webviewevaljs/MainActivity.kt
+++ app/src/main/java/xyz/mcomella/webviewevaljs/MainActivity.kt
@@ -2,26 +2,35 @@ package xyz.mcomella.webviewevaljs
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.KeyEvent
-import android.webkit.JavascriptInterface
+import android.view.View
 import android.webkit.WebChromeClient
-import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.FrameLayout
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
+    lateinit var videoContainer: FrameLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        videoContainer = fullscreenContainer
 
         webView.webViewClient = object : WebViewClient() {
         }
 
         webView.webChromeClient = object : WebChromeClient() {
+            override fun onShowCustomView(view: View?, callback: CustomViewCallback?) {
+                super.onShowCustomView(view, callback)
+                videoContainer.addView(view)
+                videoContainer.visibility = View.VISIBLE
+            }
 
+            override fun onHideCustomView() {
+                super.onHideCustomView()
+                videoContainer.removeAllViews()
+                videoContainer.visibility = View.GONE
+            }
         }
 
         webView.settings.apply {
